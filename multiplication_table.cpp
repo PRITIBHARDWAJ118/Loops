fbdd588fb0063cb08626c0ac410a0a174c423db7
Priti <pritibhardwaj118@gmail.com>
2024-08-05 23:12:10 +0530

    Add files via upload

---
+++ multiplication_table.cpp
@@ -0,0 +1,20 @@
+//priti 103
+  #include<iostream>
+using namespace std;
+int main()
+{
+    int i = 1;
+
+while (i <= 10)
+{
+    int j = 1;
+        while (j <= 10)
+        {
+        cout<<i * j <<" ";
+        j += 1;
+        }
+    cout<<endl;
+    i=i+1;
+}
+return 0;
+}
---
+++ numbers_printing.cpp
@@ -0,0 +1,15 @@
+ //priti 103
+  #include<iostream>
+using namespace std;
+int main()
+{
+   int i,a;
+   cout<<"Enter the end value : ";
+   cin>>a;
+    for (i=1;i<=a;i++)
+    {
+        cout<<i<<" ";
+    }
+    return 0;
+}
+//for(int i:array)
